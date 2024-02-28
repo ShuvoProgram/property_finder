cc4ebdc71b85bb69af71162a1d7a518602a55048
ShuvoProgram <shuvoprogramer@gmail.com>
2024-02-28 16:30:57 +0600

    Add TextAreaField component in React

---
+++ components/common/form/TextAreaField.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Form, FormText } from "react-bootstrap";
+import { useFormContext } from "react-hook-form";
+
+interface TextAreaProps {
+  labelText: string;
+  name: string;
+  placeholder?:string;
+  rows?: number;
+  errorMessage?: string;
+  labelCls?: string;
+}
+
+const TextAreaField: React.FC<TextAreaProps> = ({
+  labelText,
+  name,
+  rows,
+  placeholder,
+  errorMessage,
+  labelCls,
+}) => {
+  const { register } = useFormContext();
+
+  return (
+    <Form.Group controlId={name}>
+      <Form.Label className={labelCls ? labelCls : ""}>{labelText}</Form.Label>
+      <Form.Control
+        as="textarea"
+        rows={rows ? rows : 3}
+        {...register(name)}
+        placeholder={placeholder ? placeholder : ""}
+        className={`${errorMessage ? "is-invalid" : ""} rounded-0`}
+      />
+      {errorMessage && (
+        <FormText className="text-danger">{errorMessage}</FormText>
+      )}
+    </Form.Group>
+  );
+};
+
+export default TextAreaField;
